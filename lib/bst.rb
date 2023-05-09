cf13a6924f6736f2435af9a9c8937b199137b493
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-05-09 16:09:01 -0400

    Set basic tree structure and print method

--- lib/bst.rb
+++ lib/bst.rb
@@ -1,3 +1,4 @@
+require_relative('./helper.rb')
 class Node
     include Comparable
     attr_accessor :left_child, :right_child, :data
@@ -12,3 +13,49 @@ class Node
         @data <=> other.data
     end
 end
+
+class Tree
+    
+    def initialize(array)
+        @array = array
+        @proper_array = conditionate_array
+        @root = build_tree(@proper_array)
+    end
+
+    def conditionate_array
+        @array = merge_sort(@array)
+        @array.uniq
+    end
+
+    def build_tree(array)
+        # Sort array and remove duplicates
+       
+        # Build balance BST full of Node objects
+        # Base case
+        return nil if array.empty?
+
+        # 1.- Set root node
+        middle = array.length/2
+        root = Node.new(array[middle])
+
+        # 2.- Generate left side 
+        left_node = build_tree(array[0...middle])
+        root.left_child = left_node
+
+        # 3.- Generate right side
+        right_node = build_tree(array[middle + 1..-1])
+        root.right_child = right_node
+
+        #Return level-0 root as final step 
+        root
+    end
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+    end
+end
+
+# myTree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+# myTree.pretty_print
