939b72990bfa3ac726cc50f6fce21e42d4b51c63
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-05-13 20:45:21 -0400

    Tie it all together

--- main.rb
+++ main.rb
@@ -14,8 +14,8 @@ myTree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 # yourTree.pretty_print
 # puts 'Out'
 
-codeschool = Tree.new([12,15,5,7,3,17,13,9,1,19])
-codeschool.pretty_print
+# codeschool = Tree.new([12,15,5,7,3,17,13,9,1,19])
+# codeschool.pretty_print
 # codeschool.delete(5)
 # codeschool.pretty_print
 
@@ -31,14 +31,39 @@ codeschool.pretty_print
 
 # other_node = codeschool.find(9)
 # puts codeschool.depth(other_node)
-codeschool.delete(19)
-codeschool.delete(17)
-codeschool.delete(15)
+# codeschool.delete(19)
+# codeschool.delete(17)
+# codeschool.delete(15)
 
 
-codeschool.pretty_print
-puts codeschool.balance?
-codeschool.rebalance
-codeschool.pretty_print
-puts codeschool.balance?
+# codeschool.pretty_print
+# puts codeschool.balance?
+# codeschool.rebalance
+# codeschool.pretty_print
+# puts codeschool.balance?
+
+# Tie it all together
+
+myTree = Tree.new((Array.new(15) { rand(1..100) }))
+puts "Is it balanced? : #{myTree.balance?}"
+puts "Preorder #{myTree.preorder}"
+puts "Inorder #{myTree.inorder}"
+puts "Postorder #{myTree.postorder}"
+
+myTree.insert(101)
+myTree.insert(120)
+myTree.insert(1000)
+myTree.insert(479)
+
+puts "Is it balanced? : #{myTree.balance?}"
+myTree.rebalance
+puts "Is it balanced? : #{myTree.balance?}"
+puts "Preorder #{myTree.preorder}"
+puts "Inorder #{myTree.inorder}"
+puts "Postorder #{myTree.postorder}"
+
+myTree.pretty_print
+
+
+
 
